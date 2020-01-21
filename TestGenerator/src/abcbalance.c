3b9ced234fa00d217e788ac01ee2f3d7e3c85480
TitanGuardian <55186777+TitanGuardian@users.noreply.github.com>
2020-01-21 18:56:43 +0300

    Update abcbalance.c

--- TestGenerator/src/abcbalance.c
+++ TestGenerator/src/abcbalance.c
@@ -12,54 +12,50 @@ typedef struct Abc_Frame_t_ Abc_Frame_t;
 Abc_Frame_t * Abc_FrameGetGlobalFrame();
 int    Cmd_CommandExecute( Abc_Frame_t * pAbc, const char * sCommand );
  
- int balanceDD  () {
-     
-     
-	Abc_Frame_t * pAbc;
-	char * pFileName1= "./out/TestA.v",
+ int balanceDD  () {     
+    Abc_Frame_t * pAbc;
+    char * pFileName1= "./out/TestA.v",
          * pFileName2= "./out/TestB.v";
-	char Command[1000];
+    char Command[1000];
     int i;
     Abc_Start();
     pAbc = Abc_FrameGetGlobalFrame();
     
     sprintf(Command, "read %s", pFileName1);
     if(Cmd_CommandExecute(pAbc, Command)){
-		fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
-		return 1;
-	}
+        fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
+	return 1;
+    }
 	ARRCLEAN;
     sprintf(Command, "balance");
     if(Cmd_CommandExecute(pAbc, Command)){
-		fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
-		return 1;
-	}
-	ARRCLEAN;
+	fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
+	return 1;
+    }
+    ARRCLEAN;
     sprintf(Command, "write_verilog %s", pFileName1);
     if(Cmd_CommandExecute(pAbc, Command)){
-		fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
-		return 1;
-	}
-	ARRCLEAN;
-        sprintf(Command, "read %s", pFileName2);
+	fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
+	return 1;
+    }
+    ARRCLEAN;
+    sprintf(Command, "read %s", pFileName2);
     if(Cmd_CommandExecute(pAbc, Command)){
-		fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
-		return 1;
-	}
-	ARRCLEAN;
+	fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
+	return 1;
+    }
+    ARRCLEAN;
     sprintf(Command, "balance");
     if(Cmd_CommandExecute(pAbc, Command)){
-		fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
-		return 1;
-	}
-	ARRCLEAN;
+	fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
+	return 1;
+    }
+    ARRCLEAN;
     sprintf(Command, "write_verilog %s", pFileName2);
     if(Cmd_CommandExecute(pAbc, Command)){
-		fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
-		return 1;
-	}
+	fprintf(stdout , "Cannot execute command \"%s\".\n ",Command);
+	return 1;
+    }
     Abc_Stop();
-     
-     
     return 0;
  }
